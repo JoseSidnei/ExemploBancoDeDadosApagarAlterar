bf3ea7ff2de669fb68d234616910c3966a2a4e1d
JoseSidnei <38699945+JoseSidnei@users.noreply.github.com>
2018-06-26 15:56:20 -0300

    adicionado exemplo alterar e apagar

---
+++ ExemploBancoDeDadosAlterarApagar.sql
@@ -0,0 +1,18 @@
+﻿/* 
+Alterar
+
+UPDATE outfit SET tamanho = 'XGG' WHERE marca = 'Lelis blanc';
+
+UPDATE outfit SET cor = 'Verde', valor = 1.50
+WHERE cor = 'Rosa choque';
+
+UPDATE outfit SET marca = 'Oakley' WHERE marca = 'Okle';
+
+UPDATE outfit SET cor = 'Azel espacial' WHERE tecido LIKE '%malha%';
+ 
+*Apaga quando encontrar a cor Azul especial*
+
+DELETE FROM outfit WHERE cor = 'Azul especial'
+ */
+
+SELECT * FROM outfit 
